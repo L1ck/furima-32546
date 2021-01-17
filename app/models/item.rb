6aff1dc9ff35bc1fd9d7d0aad0dc33b731de9da4
L1ck <matsu.riku05@gmail.com>
2021-01-17 14:29:57 +0900

    activehash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :price, format: { with: /\A[0-9]+\z/ }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :state
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :area_id
+  belongs_to_active_hash :delivery_day
+
+  validates :name, :discription, :price, :category_id, :state_id, :delivery_fee_id, :area_id, :delivery_day_id, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :state_id, numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 1 }
+  validates :delivery_day_id, numericality: { other_than: 1 }
 end
--- db/migrate/20210117045750_create_items.rb
+++ db/migrate/20210117045750_create_items.rb
@@ -2,7 +2,13 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
+      t.integer       :category_id       , null: false
+      t.integer       :state_id          , null: false
+      t.integer       :delivery_fee_id   , null: false
+      t.integer       :area_id           , null: false
+      t.integer       :delivery_day_id   , null: false
       t.timestamps
     end
   end
 end
+  
