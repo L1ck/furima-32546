3d17f074fb3cacb1f9bc01d856d41d55480402e3
L1ck <matsu.riku05@gmail.com>
2021-01-21 11:38:49 +0900

    モデルコントローラー及びマイグレーション

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,28 +1,20 @@
 class ItemOrder
   include ActiveModel::Model
-  attr_accessor :token, :zip_code, :area_id, :city, :district, :building, :phone_number
+  attr_accessor :token, :zipcode, :area_id, :city, :district, :building, :phone_number, :item_id, :user_id
 
-  with_options presense: true do
-    validates :token, :area_id, :city, :district
-    validates :zip_code, format: { with: /\A[-]?[0-9]+(\.[0-9]+)?\z/ }, length: { is: 8 }
-    validates :phone_number, format: { with: /\A[0-9]+\z/ }, length: { is: 11 }
+  with_options presence: true do
+    validates :token, :city, :district
+    validates :area_id, numericality: { other_than: 1 }
+    # validates :zip_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
+    
+    with_options format: { with: /\A\d{3}[-]\d{4}\z/ } do
+      validates :zipcode, length: { is: 8 } 
+    end
   end
-# itemモデルのvalidation
-    # with_options presence: true do
-    #   validates :name, :description, :image
-
-    #   with_options numericality: { other_than: 1 } do
-    #     validates :category_id, :state_id, :shipping_fee_id, :area_id, :delivery_day_id
-    #   end
-
-    #   with_options format: { with: /\A[0-9]+\z/ } do
-    #     validates :price, numericality: { greater_than: 300, less_than: 9999999 } 
-    #   end
-    # end
 
     def save
-      # item = Item.create(name: name, price: price, image: image)
-      Address.create(zip_code: zip_code, area_id: area_id, city: city, district: district, building: building, phone_number: phone_number)
-      Order.create(user_id: user.id, item_id: item.id)
+      Address.create(zipcode: zipcode, area_id: area_id, city: city, district: district, building: building, phone_number: phone_number)
+      Order.create(user_id: user_id, item_id: item_id)
     end
 end
