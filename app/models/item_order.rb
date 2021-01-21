d53a7884bc45eaba79eca55282dfcf5b11f72550
L1ck <matsu.riku05@gmail.com>
2021-01-21 18:57:51 +0900

    修正

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -14,8 +14,8 @@ class ItemOrder
   end
 
   def save
-    Address.create(zipcode: zipcode, area_id: area_id, city: city, district: district, building: building,
-                   phone_number: phone_number)
     order = Order.create!(user_id: @user_id,item_id: item_id)
+    Address.create(zipcode: zipcode, area_id: area_id, city: city, district: district, building: building,
+                   phone_number: phone_number, order_id: order.id)
   end
 end
